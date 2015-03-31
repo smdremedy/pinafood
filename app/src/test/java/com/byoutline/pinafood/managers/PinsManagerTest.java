b328c4023faaaaf1bc5bf76fc14fee59d060b859
Sylwester Madej <sylwek.madej@gmail.com>
2015-03-31 23:56:45 +0200

    Simple unit test

---
+++ app/src/test/java/com/byoutline/pinafood/managers/PinsManagerTest.java
@@ -0,0 +1,14 @@
+package com.byoutline.pinafood.managers;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PinsManagerTest {
+
+    @Test
+    public void testName() throws Exception {
+        assertThat(new PinsManagerTest()).isNotNull();
+
+    }
+}
