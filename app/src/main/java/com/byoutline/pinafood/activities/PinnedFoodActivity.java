543ccab30b8aae61eccf9ce9a3fb5b84b156a515
Sylwester Madej <sylwek.madej@gmail.com>
2015-04-01 09:26:13 +0200

    Naw drawer

--- app/src/main/java/com/byoutline/pinafood/activities/PinnedFoodActivity.java
+++ app/src/main/java/com/byoutline/pinafood/activities/PinnedFoodActivity.java
@@ -1,28 +1,41 @@
 package com.byoutline.pinafood.activities;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.byoutline.pinafood.PinAFoodApp;
 import com.byoutline.pinafood.R;
-import com.byoutline.pinafood.managers.UserManager;
 import com.byoutline.pinafood.fragments.PinnedFoodFragment;
+import com.byoutline.pinafood.managers.UserManager;
 import com.squareup.otto.Bus;
 
 import javax.inject.Inject;
 
-import timber.log.Timber;
+public class PinnedFoodActivity extends Activity
 
-public class PinnedFoodActivity extends Activity {
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
     public static final int REQUEST_CODE = 123;
     @Inject
     UserManager userManager;
     @Inject
     Bus bus;
+
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
+    private CharSequence mTitle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +51,15 @@ public class PinnedFoodActivity extends Activity {
         }
         setContentView(R.layout.activity_pinned_food);
 
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
+
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new PinnedFoodFragment())
@@ -50,6 +72,31 @@ public class PinnedFoodActivity extends Activity {
         getMenuInflater().inflate(R.menu.pinned_food, menu);
         return true;
     }
+
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, new PinnedFoodFragment())
+                .commit();
+    }
+
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                mTitle = getString(R.string.title_section1);
+                break;
+            case 2:
+                mTitle = getString(R.string.title_section2);
+                break;
+            case 3:
+                mTitle = getString(R.string.title_section3);
+                break;
+        }
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
